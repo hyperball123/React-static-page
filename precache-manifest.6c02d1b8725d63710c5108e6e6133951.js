8fa31d92a6e4c7e7383cd6c223b6341b8c93ddc3
Sufiyan Shaikh <sufiwork2001@gmail.com>
2023-09-07 16:21:15 +0530

    Updates

---
+++ precache-manifest.6c02d1b8725d63710c5108e6e6133951.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "8947533eb7525a4be443",
+    "url": "/React-static-page/static/css/main.6cc5eb5e.chunk.css"
+  },
+  {
+    "revision": "8947533eb7525a4be443",
+    "url": "/React-static-page/static/js/main.44496e49.chunk.js"
+  },
+  {
+    "revision": "9c1666592c44a5a9d0bd",
+    "url": "/React-static-page/static/js/runtime~main.b52e95ed.js"
+  },
+  {
+    "revision": "baf0bee18a4a038fd05e",
+    "url": "/React-static-page/static/js/2.e5d5fb34.chunk.js"
+  },
+  {
+    "revision": "629666658164b43f59b6",
+    "url": "/React-static-page/static/js/3.45bd1f93.chunk.js"
+  },
+  {
+    "revision": "ba02ec7b3d2f92c4e86c1b11bf5228b1",
+    "url": "/React-static-page/static/media/image1.ba02ec7b.jpg"
+  },
+  {
+    "revision": "ca1623f0a6c6890dcf4154980f349f41",
+    "url": "/React-static-page/static/media/image2.ca1623f0.jpg"
+  },
+  {
+    "revision": "26af824e73eef023c34de4516af7c80f",
+    "url": "/React-static-page/static/media/image3.26af824e.jpg"
+  },
+  {
+    "revision": "6ea0466e5f0f17bf66acce22403a622c",
+    "url": "/React-static-page/static/media/about1.6ea0466e.jpg"
+  },
+  {
+    "revision": "02d1d642578e6e37c69f03e0c97d0312",
+    "url": "/React-static-page/static/media/about2.02d1d642.jpg"
+  },
+  {
+    "revision": "e173e3c2b578fbdd340f37dd3762328d",
+    "url": "/React-static-page/static/media/home-studio.e173e3c2.jpg"
+  },
+  {
+    "revision": "2ee99d17c6148b08ee7f6022808b1af3",
+    "url": "/React-static-page/static/media/about-studio.2ee99d17.jpg"
+  },
+  {
+    "revision": "5a89fb6be256e805cf3f22579bcbf4dd",
+    "url": "/React-static-page/index.html"
+  }
+];
